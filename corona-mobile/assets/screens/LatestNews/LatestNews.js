0c1e872574c1ead000fd813d3ee94f2aa1f77a57
Michael Ginn <milespergallon271@gmail.com>
2020-03-15 22:48:03 -0500

    refactor LatestNews to dynamically generate buttons

--- corona-mobile/assets/screens/LatestNews/LatestNews.js
+++ corona-mobile/assets/screens/LatestNews/LatestNews.js
@@ -9,94 +9,33 @@ import {
 } from 'react-native';
 
 export default function LatestNews({ navigation }) {
+  const sites = [
+    { title: "CNN", source: "https://www.cnn.com/search?size=10&q=coronavirus" },
+    { title: "BBC", source: "https://www.cnn.com/search?size=10&q=coronavirus" },
+    { title: "NYT", source: "https://www.cnn.com/search?size=10&q=coronavirus" },
+    { title: "ALJAZEERA", source: "https://www.cnn.com/search?size=10&q=coronavirus" },
+    { title: "REUTERS", source: "https://www.cnn.com/search?size=10&q=coronavirus" },
+    { title: "CNBC", source: "https://www.cnn.com/search?size=10&q=coronavirus" },
+    { title: "WHO", source: "https://www.cnn.com/search?size=10&q=coronavirus" },
+    { title: "SKY", source: "https://www.cnn.com/search?size=10&q=coronavirus" },
+  ]
     return (
       <View style={styles.container}>
         <Text style={styles.title}>
           Latest News
         </Text>
-        
-        <Button 
-          title="CNN" 
-          onPress={() => {
-            navigation.navigate('NewsScreen', {
-                site: 'CNN'
-              }
-            );
-          }}
-        />
 
-        <Button 
-          title="BBC" 
-          onPress={() => {
-            navigation.navigate('NewsScreen', {
-                site: 'BBC'
+        {sites.map(site => {
+          return (<Button
+            title={site.title}
+            onPress={() => {
+              navigation.navigate('NewsScreen', {
+                site: site
               }
-            );
-          }}
-        />
-
-        <Button 
-          title="NYT" 
-          onPress={() => {
-            navigation.navigate('NewsScreen', {
-                site: 'NYT'
-              }
-            );
-          }}
-        />
-
-        <Button 
-          title="ALJAZEERA" 
-          onPress={() => {
-            navigation.navigate('NewsScreen', {
-                site: 'ALJAZEERA'
-              }
-            );
-          }}
-        />
-
-        <Button 
-          title="REUTERS" 
-          onPress={() => {
-            navigation.navigate('NewsScreen', {
-                site: 'REUTERS'
-              }
-            );
-          }}
-        />
-
-        <Button 
-          title="CNBC" 
-          onPress={() => {
-            navigation.navigate('NewsScreen', {
-                site: 'CNBC'
-              }
-            );
-          }}
-        />
-
-        <Button 
-          title="WHO" 
-          onPress={() => {
-            navigation.navigate('NewsScreen', {
-                site: 'WHO'
-              }
-            );
-          }}
-        />
-
-        <Button 
-          title="SKY" 
-          onPress={() => {
-            navigation.navigate('NewsScreen', {
-                site: 'SKY'
-              }
-            );
-          }}
-        />
-
-        
-        
+              );
+            }}
+          />);
+        })}
 
       </View>
     );
