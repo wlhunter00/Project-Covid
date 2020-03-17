45406c82b314684e1c221a74f0358bc442c84d0c
Adam J <jacobowitz.ad@gmail.com>
2020-03-17 17:59:47 -0400

    Add testingCenter functionality

--- backend/routes/testingCenter.js
+++ backend/routes/testingCenter.js
@@ -2,10 +2,46 @@ const express = require("express");
 const router = express.Router();
 const Notification = require("./../models/notification.js");
 
-router.post("/", async (req, res) => {
-  var location = req.body;
+router.get("/testCenter", async (req, res) => {
+  var serverLocation = req.body;
   console.log(location);
-  res.send(location);
+
+  var coords = parseCoord(serverLocation)
+
+  const url = "http://www.mapquestapi.com/geocoding/v1/reverse?key=5FAG0NhAjLLNkkvmLKhMfzSvqQcEhING&" + coords[0] + "," + coords[1];
+  await request(url, function(
+    error,
+    response,
+    body
+  ){
+    var data = JSON.parse(results);
+    var address = data["results"][0]["locations"][0]["adminArea3"]
+    console.log(address)
+  });
+
+  JSON.parse(./)
+
+  await request()
+
+  res.send(state);
 });
+
+
+function parseCoord(request){
+  try{
+    var loc = JSON.parse(request);
+    var latitude = loc["location"]["coords"]["latitude"]
+    var longitude = loc["location"]["coords"]["longitude"]
+    return [latitude,longitude]
+  } catch(err){
+    console.error(err)
+  }
+}
+
+function findState(loctions){
+
+}
+
+
 
 module.exports = router;
