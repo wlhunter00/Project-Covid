575c85853dfb82ffd8e19a730ec879faa3b9595f
chukwuemekaajah <talk2ajah@gmail.com>
2020-03-17 11:56:54 +0100

    first commit on the web push notifications service

--- corona-push-notifications/frontend/src/components/index.jsx
+++ corona-push-notifications/frontend/src/components/index.jsx
@@ -9,48 +9,7 @@ import {connect} from "react-redux";
 
 
 class Home extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = { news:[]};
-        this.dispatchStateChange = this.dispatchStateChange.bind(this);
-    }
-
-    componentDidMount(){
-        ( async () => {
-            try{
-                let response = await axios.get(`${API_ROUTE}/news`);
-                if(response.data.error){
-                    alert("We could not fetch new news at this time.")
-                    return;
-                }
-                return this.setState({news:response.data}, () => {
-                   this.dispatchStateChange(response.data);
-                })
-                
-            } catch(err){
-                this.setState({loading:false});
-            }
-        })();
-
-    }
-
-    componentDidUpdate(previousProps, previousState){
-        console.log(previousProps.hash, this.props.hash);
-        if(previousProps.hash != this.props.hash){
-            this.setState({news : this.props.news});
-            return true;
-        }
-    }
-
-    dispatchStateChange(data){
-        this.props.dispatch({
-            type:"SET_NEWS",
-            payload: {hash: Date.now().toString(16), news: data},
-        })
-    }
-
     
-
     render() {
         
         return (
@@ -62,10 +21,5 @@ class Home extends React.Component {
     }
 }
 
-function mapStateToProps(state){
-    return {
-        ...state,
-    }
-}
 
-export default connect(mapStateToProps)(Home);
+export default connect()(Home);
