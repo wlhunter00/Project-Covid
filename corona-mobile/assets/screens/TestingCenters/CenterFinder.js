97626fe52251e3f255b857bdd8a8fdb2c1e4622f
giorgioguttilla <gioguttilla@gmail.com>
2020-03-18 11:00:47 -0400

    Testing center page functional

--- corona-mobile/assets/screens/TestingCenters/CenterFinder.js
+++ corona-mobile/assets/screens/TestingCenters/CenterFinder.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { StyleSheet, Text, View, TouchableOpacity, Linking } from "react-native";
 import { ActionButton } from '../../components/Buttons';
 import { styles } from "../../styles/styles";
+import { WebView } from 'react-native-webview';
 
 export default function Diagnosis({ route, navigation }) {
   const { locators } = route.params;
@@ -24,10 +25,21 @@ export default function Diagnosis({ route, navigation }) {
       <Text>{route.params.state}</Text>
 
       <Text>{route.params.department}</Text>
-      <View style={styles.containerRowCenter}>
+      <View style={styles.container}>
         <ActionButton title='Call Center' action={call}/>
 
-        <ActionButton title='Website' action={openUrl}/>
+        {/* <ActionButton title='Website' action={openUrl}/> */}
+
+        <WebView
+          originWhitelist={["*"]}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          startInLoadingState={true}
+          source={{
+            uri: route.params.url
+          }}
+          style={styles.webView}
+        />
       </View>
     </View>
   );
--- corona-mobile/assets/screens/TestingCenters/TestingCenters.js
+++ corona-mobile/assets/screens/TestingCenters/TestingCenters.js
@@ -1,47 +1,82 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, ActivityIndicator } from "react-native";
 
 import { getLocationAsync } from "../../Utils";
 
-export default function TestingCenters({ navigation }) {
-  const locResp = getLocationAsync().then(locResp => {
-    // console.log(locResp);
-    const lat = locResp.location.coords.latitude;
-    const long = locResp.location.coords.longitude;
-    console.log(lat, long);
-    fetch("https://projectcovid-backend.herokuapp.com/centers/", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(locResp)
-    })
-      .then(response => response.json())
-      .then(responseJson => {
-        console.log(responseJson);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  });
+export default class TestingCenters extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoaded: false,
+      location: {}
+    }
+  }
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Testing Centers</Text>
-      <Button
-        title="Find Center"
-        onPress={() => navigation.push("CenterFinder", {
-          state: 'Alaska',
-          department: 'Alaska Department of Health and Social Services',
-          tel: '0123456789',
-          url: 'http://dhss.alaska.gov/dph/Epi/id/Pages/COVID-19/general.aspx'
-        })}
-      />
-    </View>
-  );
+  componentDidMount(){
+    const locResp = getLocationAsync().then(locResp => {
+      // console.log(locResp);
+      const lat = locResp.location.coords.latitude;
+      const long = locResp.location.coords.longitude;
+      console.log(lat, long);
+      fetch("https://projectcovid-backend.herokuapp.com/centers/", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(locResp)
+      })
+        .then(response => response.json())
+        .then(responseJson => {
+          this.setState({isLoaded: true, location: responseJson})
+          console.log(this.state.location);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    });
+  }
+
+  render(){
+
+    if(this.state.isLoaded === false){
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator size='small'/>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          {this.props.navigation.push("CenterFinder", {
+            state: this.state.location.State,
+            department: this.state.location["State Department"],
+            tel: this.state.location["Phone Number"],
+            url: this.state.location.URL
+          })}
+        </View>
+      );
+    }
+
+    // return (
+    //   <View style={styles.container}>
+    //     <Text style={styles.title}>Testing Centers</Text>
+    //     {/* <Button
+    //       title="Find Center"
+    //       onPress={() => this.props.navigation.push("CenterFinder", {
+    //         state: 'Alaska',
+    //         department: 'Alaska Department of Health and Social Services',
+    //         tel: '0123456789',
+    //         url: 'http://dhss.alaska.gov/dph/Epi/id/Pages/COVID-19/general.aspx'
+    //       })}
+    //     /> */}
+
+    //   </View>
+    // );
+  }
 }
 
 const styles = StyleSheet.create({
