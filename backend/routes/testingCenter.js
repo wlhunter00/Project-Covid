898f5955239de1262a78a3b73207c36298778e9a
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-03-17 20:44:40 -0400

    tried to fix async await issue and failed

--- backend/routes/testingCenter.js
+++ backend/routes/testingCenter.js
@@ -1,30 +1,37 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-const CsvReadableStream = require("csv-reader");
-
-const ABBREV = "Abbreviation";
-const csv = require("csv-parser");
 var request = require("request-promise");
+const neatCsv = require("neat-csv");
+
+// ISSUE: ASYNC issue. At line 23 we are asking for state info and to parse a csv, it parses the csv but returns before the parsing is done.
 
 router.post("/", async (req, res) => {
   var serverLocation = req.body;
   var coords = parseCoord(serverLocation);
-
   const MPurl =
     "http://www.mapquestapi.com/geocoding/v1/reverse?key=5FAG0NhAjLLNkkvmLKhMfzSvqQcEhING&location=" +
     coords[0] +
     "," +
     coords[1];
-
-  request(MPurl, async function(error, response, body) {
-    var data = JSON.parse(body);
-    address = data["results"][0]["locations"][0]["adminArea3"];
-    console.log(address);
-    let stateInfo = await getStateInfo(address);
-    console.log(stateInfo);
-    res.send(address);
-  });
+  try {
+    await request(MPurl, async function(err, response, body) {
+      var data = JSON.parse(body);
+      let address = await data["results"][0]["locations"][0]["adminArea3"];
+      console.log(address);
+      try {
+        // we are awaiting here but its not working
+        let stateInfo = await getStateInfo(address);
+        console.log(stateInfo);
+        // res.send(address);
+      } catch (err) {
+        console.log(err);
+      }
+      res.send(address);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 function parseCoord(loc) {
@@ -40,19 +47,26 @@ function parseCoord(loc) {
 }
 
 async function getStateInfo(address) {
-  var readStream = fs
-    .createReadStream("./data/test-center.csv")
-    .pipe(csv())
-    .on("data", function(row) {
-      try {
-        if (row[ABBREV] == address) {
-          readStream.destroy();
-          return row;
-        }
-      } catch (err) {
-        console.log(err);
+  const file = fs.readFile("./data/test-center.csv", async (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    // We are awaiting here
+    const allInfo = await neatCsv(data);
+
+    // parse the csv
+    for (var i = 0; i < allInfo.length; ++i) {
+      if (allInfo[i].Abbreviation === address) {
+        const stateInfo = allInfo[i];
+        // Console logging works
+        console.log(stateInfo);
+        // but then returning doesn't
+        return stateInfo;
       }
-    });
+    }
+  });
 }
 
 module.exports = router;
