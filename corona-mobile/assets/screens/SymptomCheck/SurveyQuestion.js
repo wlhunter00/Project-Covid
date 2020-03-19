4200205b2588e3d467295e35017b8e9ee6ff9f2d
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-03-19 17:31:10 -0400

    fixed comment in symptom check so that it works

--- corona-mobile/assets/screens/SymptomCheck/SurveyQuestion.js
+++ corona-mobile/assets/screens/SymptomCheck/SurveyQuestion.js
@@ -1,63 +1,68 @@
 import * as React from "react";
-import { Text, View, TouchableHighlight, TouchableOpacity, Image } from "react-native";
-import { SurveyButton } from './../../components/Buttons';
-import { ResponseTextBox } from './../../components/TextBoxes';
+import {
+  Text,
+  View,
+  TouchableHighlight,
+  TouchableOpacity,
+  Image
+} from "react-native";
+import { SurveyButton } from "./../../components/Buttons";
+import { ResponseTextBox } from "./../../components/TextBoxes";
 import { styles } from "../../styles/styles";
 
-export function MultipleChoiceQuestion({ prompt, answers, currResponse, saveAnswer }) {
-    const [currAnswer, changeAnswer] = React.useState("");
-    const setAnswer = React.useCallback(
-        (answer) => (event) => {
-            saveAnswer(currResponse + answer + " " );
-        },
-        []
-    );
-    return (
+export function MultipleChoiceQuestion({
+  prompt,
+  answers,
+  currResponse,
+  saveAnswer
+}) {
+  const [currAnswer, changeAnswer] = React.useState("");
+  const setAnswer = React.useCallback(
+    answer => event => {
+      saveAnswer(currResponse + answer + " ");
+    },
+    []
+  );
+  return (
+    <View>
+      <View style={{ marginLeft: 10 }}>
+        <Text style={styles.navButtonTitle}>{prompt}</Text>
         <View>
-            <View style={{ marginLeft: 10 }}>
-                <Text style={styles.navButtonTitle}>{prompt}</Text>
-                <View>
-                    {answers.map(function (answer) {
-                        return <SurveyButton title={answer} action={setAnswer(answer)}/>
-                    })}
-                </View>
-            </View>
+          {answers.map(function(answer) {
+            return <SurveyButton title={answer} action={setAnswer(answer)} />;
+          })}
         </View>
-    );
+      </View>
+    </View>
+  );
 }
 
 export function ShortAnswerQuestion({ prompt, currResponse, saveAnswer }) {
-    const [currAnswer, changeAnswer] = React.useState("");
+  const [currAnswer, changeAnswer] = React.useState("");
 
-    const setAnswer = React.useCallback(
-         (event) => {
-            {event.target &&
-                changeAnswer(currAnswer => event.target.value);
-                event.persist();
-            }
-        },
-        []
-    );
+  const setAnswer = React.useCallback(event => {
+    {
+      event.target && changeAnswer(currAnswer => event.target.value);
+      event.persist();
+    }
+  }, []);
 
-    const recordAnswer = React.useCallback(
-        (answer) => (event) => {
-            saveAnswer(currResponse + answer + " ");
-        },
-        []
-    );
-    return (
-        <View>
-            <View style={{ marginLeft: 10 }}>
-                <Text style={styles.navButtonTitle}>{prompt}</Text>
-                <View>
-                    {/* <input
-                        type="text"
-                        onChange={setAnswer}
-                    /> */}
-                    <ResponseTextBox defaultText="" changeFunction={setAnswer} />
-                </View>
-                <SurveyButton title="Continue" action={recordAnswer(currAnswer)} />
-            </View>
-        </View>
-    );
+  const recordAnswer = React.useCallback(
+    answer => event => {
+      saveAnswer(currResponse + answer + " ");
+    },
+    []
+  );
+  return (
+    <View>
+      <View style={{ marginLeft: 10 }}>
+        <Text style={styles.navButtonTitle}>{prompt}</Text>
+        <SurveyButton title="Continue" action={recordAnswer(currAnswer)} />
+      </View>
+    </View>
+  );
 }
+// <View>
+//   {<input type="text" onChange={setAnswer} />}
+//   <ResponseTextBox defaultText="" changeFunction={setAnswer} />
+// </View>
