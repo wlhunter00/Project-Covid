8e294729b6fcd5d928204051b6bc7778392c02a3
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-03-20 18:46:31 -0400

    backend for symptoms finished

--- backend/routes/symptomCheck.js
+++ backend/routes/symptomCheck.js
@@ -1,19 +1,47 @@
 const express = require("express");
 const router = express.Router();
 const spawn = require("child_process").spawn;
+const symptomData = require("./../data/Symptom-Percentages.json");
 const nlpScriptRoute =
   __dirname.slice(0, __dirname.lastIndexOf("\\")) + "/scripts/testScript.py";
+
+function getSympInfo(symptom) {
+  for (const prop in symptomData) {
+    if (symptomData[prop].Symptom === symptom) {
+      // return JSON.stringify(symptomData[prop]);
+      return symptomData[prop];
+    }
+  }
+  var notSymp = { message: "No data regarding the symptom found!" };
+  return notSymp;
+}
 
 router.post("/", async (req, res) => {
   var symptoms = req.body.symptoms;
   var nlpScript = spawn("python", [nlpScriptRoute, symptoms]);
   nlpScript.stdout.on("data", function(data) {
-    console.log(data.toString());
-    res.send(data.toString());
+    var pythonReturn = data.toString();
+    var responseList = [];
+    var sympList = pythonReturn.split("\r\n");
+    if (
+      sympList === undefined ||
+      sympList.length == 0 ||
+      sympList[0] === "No matches found"
+    ) {
+      return res.send({
+        message: "No Symptoms found"
+      });
+    }
+    sympList.pop();
+    var jsonArray = [];
+    for (symp in sympList) {
+      jsonArray.push(getSympInfo(sympList[symp]));
+    }
+    return res.send(jsonArray);
   });
   nlpScript.stderr.on("data", data => {
     console.log("error: ", data.toString());
-    res.status(400).send(data.toString());
+    return res.status(400).send(data.toString());
   });
 });
 
--- backend/scripts/testScript.py
+++ backend/scripts/testScript.py
@@ -1,5 +1,6 @@
 import sys
 # Takes first name and last name via command
 # line arguments and then display them
-print("Output from Python")
-print("First input: " + sys.argv[1])
+print("Cough")
+print("Chills")
+print("Nausea")
