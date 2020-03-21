d0c9eab2cc7c7ef524bdb891ab6755bffc3b9588
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-03-21 16:03:40 -0400

    debugging

--- backend/routes/symptomCheck.js
+++ backend/routes/symptomCheck.js
@@ -20,9 +20,9 @@ router.post("/", async (req, res) => {
   if (!symptoms) {
     return res.status(400).send("No text sent!");
   }
-  return res.send(jsonPath);
   var nlpScript = spawn("python", [nlpScriptRoute, symptoms, jsonPath]);
   nlpScript.stdout.on("data", function(data) {
+    return res.send(data);
     var pythonReturn = data.toString();
     var responseList = [];
     var sympList = pythonReturn.split(";");
