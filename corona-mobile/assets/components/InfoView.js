468c6f208fa57513e6d65048472eba20ad34cc81
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-03-21 20:14:57 -0400

    infoviewdiagnosis update

--- corona-mobile/assets/components/InfoView.js
+++ corona-mobile/assets/components/InfoView.js
@@ -7,16 +7,13 @@ import {
   Image,
   StyleSheet
 } from "react-native";
-import { Entypo } from "@expo/vector-icons";
 import { styles, defaults, boxStyles } from "../styles/styles";
 import { PrimaryTextBold } from "./Texts";
 
 export function InfoView({ title, body }) {
   return (
     <View style={boxStyles.container}>
-      <Text style={boxStyles.compactName}>
-        {title}
-      </Text>
+      <Text style={boxStyles.compactName}>{title}</Text>
       <View style={boxStyles.divider} />
       <View style={boxStyles.bio}>{body}</View>
     </View>
--- corona-mobile/assets/screens/SymptomCheck/Diagnosis.js
+++ corona-mobile/assets/screens/SymptomCheck/Diagnosis.js
@@ -1,58 +1,85 @@
-
 import * as React from "react";
 import { StyleSheet, Text, View, Button, ScrollView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { styles, boxStyles } from "./../../styles/styles.js";
-import { SurveyNavigationButton, SurveyNavigationButtonLarge, SurveyButtonSmall } from '../../components/Buttons';
-import { InfoViewDiagnosis } from './../../components/InfoViewDiagnosis';
-
+import {
+  SurveyNavigationButton,
+  SurveyNavigationButtonLarge,
+  SurveyButtonSmall
+} from "../../components/Buttons";
+import { InfoViewDiagnosis } from "./../../components/InfoViewDiagnosis";
 
 var symptoms = [];
 
 var a;
 
-export function Diagnosis({navigation, response, retakeSurvey}) {
+export function Diagnosis({ navigation, response, retakeSurvey }) {
   for (a in response) {
     if (response[a]["Percentage"]) {
-      symptoms.push({ title: response[a]["Symptom"], body: response[a]["Percentage"] });
+      symptoms.push({
+        title: response[a]["Symptom"],
+        body: response[a]["Percentage"]
+      });
     }
   }
   if (response == "") {
     symptoms = [];
   }
-    return (
+  return (
     <ScrollView>
       <Text style={styles.sectionTitleCenter}> Diagnosis Results</Text>
       <ScrollView style={styles.container}>
         {symptoms.map(symptom => {
-        return (
-          <InfoViewDiagnosis title={symptom.title} body={symptom.body + "% of people who tested positive have this symptom."}  />
-        );
-      })}
+          return (
+            <InfoViewDiagnosis
+              title={symptom.title}
+              body={
+                symptom.body +
+                "% of people who tested positive have this symptom."
+              }
+            />
+          );
+        })}
       </ScrollView>
-    <View style={{ marginTop: 10, marginRight: 10, marginLeft: 10 }}>
-    <View style={boxStyles.container}>
-          <Text style={styles.surveyQuestionText}>Based on your symptoms, we recommend utlizing the following knowledge resources:</Text>
-            <View style={{ flexDirection: "row" }}>
-              <View style={{ flex: 1 }}>
-                <SurveyNavigationButton title={"Info Toolkit"} navigation={navigation} navigationName={"InformationalToolkit"} />
-              </View>
-              <View style={{ flex: 1 }}>
-                <SurveyNavigationButton title={"Best Practices"} navigation={navigation} navigationName={"PreventativePractices"} />
-              </View>
+      <View style={{ marginTop: 10, marginRight: 10, marginLeft: 10 }}>
+        <View style={boxStyles.container}>
+          <Text style={styles.surveyQuestionText}>
+            Based on your symptoms, we recommend utlizing the following
+            knowledge resources:
+          </Text>
+          <View style={{ flexDirection: "row" }}>
+            <View style={{ flex: 1 }}>
+              <SurveyNavigationButton
+                title={"Info Toolkit"}
+                navigation={navigation}
+                navigationName={"InformationalToolkit"}
+              />
             </View>
+            <View style={{ flex: 1 }}>
+              <SurveyNavigationButton
+                title={"Best Practices"}
+                navigation={navigation}
+                navigationName={"PreventativePractices"}
+              />
+            </View>
+          </View>
+        </View>
       </View>
-    </View>
-    <View style={{ marginTop: 10, marginRight: 10, marginLeft: 10 }}>
-      <View style={boxStyles.container}>
-      <Text style={styles.surveyQuestionText}>You can also find local information on COVID-19 data, and find testing centers for help:</Text>
-            <SurveyNavigationButtonLarge title={"Find Center"} navigation={navigation} navigationName={"TestingCenters"} />
+      <View style={{ marginTop: 10, marginRight: 10, marginLeft: 10 }}>
+        <View style={boxStyles.container}>
+          <Text style={styles.surveyQuestionText}>
+            You can also find local information on COVID-19 data, and find
+            testing centers for help:
+          </Text>
+          <SurveyNavigationButtonLarge
+            title={"Find Center"}
+            navigation={navigation}
+            navigationName={"TestingCenters"}
+          />
+        </View>
       </View>
-    </View >
-    <SurveyButtonSmall title="Retake Symptom Check" action={retakeSurvey} />
+      <SurveyButtonSmall title="Retake Symptom Check" action={retakeSurvey} />
     </ScrollView>
   );
-
 }
-
