9162988813e6a4a8671c7361f9c09ade94c5240d
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-03-24 15:52:41 -0400

    updated news sources

--- corona-mobile/assets/screens/LatestNews/LatestNews.js
+++ corona-mobile/assets/screens/LatestNews/LatestNews.js
@@ -10,14 +10,24 @@ export default function LatestNews({ navigation }) {
   const { styles } = useStyle("container");
   const sites = [
     {
+      title: "CDC",
+      source: "https://www.cdc.gov/coronavirus/2019-ncov/whats-new-all.html",
+      imagesrc: require("./../../images/newsLogos/cdc.png")
+    },
+    {
       title: "WHO",
       source: "https://www.who.int/emergencies/diseases/novel-coronavirus-2019",
       imagesrc: require("./../../images/newsLogos/who.png")
     },
+    // {
+    //   title: "CNN",
+    //   source: "https://www.cnn.com/search?size=10&q=coronavirus",
+    //   imagesrc: require("./../../images/newsLogos/cnn.jpg")
+    // },
     {
-      title: "CNN",
-      source: "https://www.cnn.com/search?size=10&q=coronavirus",
-      imagesrc: require("./../../images/newsLogos/cnn.jpg")
+      title: "Associated Press",
+      source: "https://apnews.com/VirusOutbreak",
+      imagesrc: require("./../../images/newsLogos/ap.png")
     },
     {
       title: "BBC News",
@@ -25,9 +35,9 @@ export default function LatestNews({ navigation }) {
       imagesrc: require("./../../images/newsLogos/bbc.png")
     },
     {
-      title: "New York Times",
-      source: "https://www.nytimes.com/search?query=coronavirus",
-      imagesrc: require("./../../images/newsLogos/nyt.jpg")
+      title: "National Public Radio",
+      source: "https://www.npr.org/series/812054919/the-coronavirus-crisis",
+      imagesrc: require("./../../images/newsLogos/npr.png")
     },
     {
       title: "Aljazeera",
@@ -41,21 +51,26 @@ export default function LatestNews({ navigation }) {
       imagesrc: require("./../../images/newsLogos/Reuters.jpg")
     },
     {
-      title: "CNBC",
-      source:
-        "https://www.cnbc.com/search/?query=coronavirus&qsearchterm=coronavirus",
-      imagesrc: require("./../../images/newsLogos/cnbc.png")
+      title: "New York Times",
+      source: "https://www.nytimes.com/search?query=coronavirus",
+      imagesrc: require("./../../images/newsLogos/nyt.jpg")
     },
     {
-      title: "Sky News",
-      source: "https://news.sky.com/topic/coronavirus-8483",
-      imagesrc: require("./../../images/newsLogos/sky.png")
-    },
-    {
-      title: "CDC",
-      source: "https://www.cdc.gov/coronavirus/2019-ncov/whats-new-all.html",
-      imagesrc: require("./../../images/newsLogos/cdc.png")
+      title: "Bloomberg",
+      source: "https://www.bloomberg.com/",
+      imagesrc: require("./../../images/newsLogos/bloom.jpg")
     }
+    // {
+    //   title: "CNBC",
+    //   source:
+    //     "https://www.cnbc.com/search/?query=coronavirus&qsearchterm=coronavirus",
+    //   imagesrc: require("./../../images/newsLogos/cnbc.png")
+    // },
+    // {
+    //   title: "Sky News",
+    //   source: "https://news.sky.com/topic/coronavirus-8483",
+    //   imagesrc: require("./../../images/newsLogos/sky.png")
+    // },
   ];
   return (
     <ScrollView style={styles.container}>
