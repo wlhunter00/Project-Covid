627dda37edbaba1e37b701099d7ac45687f6fae8
Michael Ginn <milespergallon271@gmail.com>
2020-03-28 19:19:27 -0500

    fix status bar

--- corona-mobile/App.js
+++ corona-mobile/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, StatusBar } from "react-native";
 import { AppearanceProvider } from "react-native-appearance";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -223,6 +223,7 @@ export default function App() {
 
   return (
     <AppearanceProvider>
+      <StatusBar barStyle={isDark ? "light-content" : "dark-content"}/>
       <NavigationContainer>
         <Tab.Navigator
           screenOptions={({ route }) => ({
