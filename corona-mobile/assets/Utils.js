9da448a589593791ee5118a4b4c2c4dd7c52bdad
Michael Ginn <milespergallon271@gmail.com>
2020-03-30 01:37:49 -0500

    add interval hook if we ever need

--- corona-mobile/assets/Utils.js
+++ corona-mobile/assets/Utils.js
@@ -9,3 +9,25 @@ export async function getLocationAsync() {
   let location = await Location.getCurrentPositionAsync({});
   return { location };
 }
+
+// Shamelessly stolen from https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+// Will eventually use to update live stats as needed
+export function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
