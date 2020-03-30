4510010d6564b3b9dcacc617c429f5e41e7c2a34
Michael Ginn <milespergallon271@gmail.com>
2020-03-30 12:45:43 -0500

    stop stats fetch from happening infinitely

--- corona-mobile/assets/screens/HomeScreen.js
+++ corona-mobile/assets/screens/HomeScreen.js
@@ -46,7 +46,7 @@ export default function HomeScreen({ navigation }) {
       }
     }
     fetchStats();
-  });
+  }, []);
 
   return (
     <ScrollView style={styles.container} contentContainerStyle={{ paddingHorizontal: 15 }}>
