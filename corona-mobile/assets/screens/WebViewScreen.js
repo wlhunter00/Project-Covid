c38f97b7cf55030bad6e292a4cfe4eb698954dcd
giorgioguttilla <gioguttilla@gmail.com>
2020-04-08 15:09:38 -0400

    web view screen now has navigation/sharing

--- corona-mobile/assets/screens/WebViewScreen.js
+++ corona-mobile/assets/screens/WebViewScreen.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Share } from "react-native";
 import { WebView } from "react-native-webview";
 import { useStyle } from "../styles/styles";
+import {
+  Entypo
+} from "@expo/vector-icons";
+import { TouchableHighlight } from "react-native-gesture-handler";
 var URL = require('url');
 
 
@@ -11,28 +15,93 @@ function ConstructOriginWhitelist(urlString){
   return [parsedUrl.protocol + "//" + parsedUrl.host + "*"];
 }
 
-
-export default function WebViewScreen({ route, navigation }) {
+export default function StyledWebViewScreen({route, navigation}){
   const { styles } = useStyle("container");
-  const { url, title, originWhitelist } = route.params;
-  navigation.setOptions({ title: title })
+  return <WebViewScreen route={route} navigation={navigation} styles={styles} />;
+}
 
-  console.log(route.params)
+class WebViewScreen extends React.Component {
+  constructor(props){
+    super(props);
+    console.log(this.props);
 
-  // var activeWhitelist = (originWhitelist === undefined) ? ConstructOriginWhitelist(url) : originWhitelist;
-  var activeWhitelist = (originWhitelist === undefined) ? ["*"] : originWhitelist;
+    this.props.navigation.setOptions({ title: this.props.route.params.title });
+    this.webViewRef = React.createRef();
+    this.activeWhitelist = (this.props.route.params.originWhitelist === undefined) ? ["*"] : originWhitelist;
+    
+    this.state = {
+      canGoBack: false,
+      canGoForward: false,
+      currentUrl: this.props.route.params.url
+    }
+  }
 
-  return (
-    <View style={styles.container}>
-      <WebView
-        source={{ uri: url }}
-        style={styles.container}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-        startInLoadingState={true}
-        originWhitelist={activeWhitelist}
-        mediaPlaybackRequiresUserAction={true}
-      />
-    </View>
-  );
+  sharePage = async () => {
+    try {
+      const result = await Share.share({
+        url: this.state.currentUrl
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+
+        } else {
+
+        }
+      } else if (result.action === Share.dismissedAction) {
+
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+  
+  render() {
+    return(
+      <View style={this.props.styles.container}>
+        <WebView
+          source={{ uri: this.props.route.params.url }}
+          style={this.props.styles.container}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          startInLoadingState={true}
+          originWhitelist={this.activeWhitelist}
+          mediaPlaybackRequiresUserAction={true}
+          ref={this.webViewRef}
+          onNavigationStateChange={state => {
+            this.setState({
+              canGoBack: state.canGoBack, 
+              canGoForward: state.canGoForward,
+              currentUrl: state.url
+            })
+          }}
+        />
+        <View style={{flex: 1, flexDirection: 'row', justifyContent:'space-between', maxHeight: 40, backgroundColor: 'green'}}>
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <TouchableHighlight 
+              disabled={!this.state.canGoBack} 
+              onPress={() => {this.webViewRef.current.goBack()}}
+            >
+              <Entypo name='chevron-left' size={40} color={this.state.canGoBack ? 'white' : 'grey'}/>
+            </TouchableHighlight>
+            <TouchableHighlight 
+              disabled={!this.state.canGoForward}
+              onPress={() => {this.webViewRef.current.goForward()}}
+            >
+              <Entypo name='chevron-right' size={40} color={this.state.canGoForward ? 'white' : 'grey'}/>
+            </TouchableHighlight>
+          </View>
+
+          <TouchableHighlight style={{flex: 1}} onPress={() => {this.sharePage(this.state.currentUrl)}}>
+            <Text style={{flex: 1, color: 'white', fontSize: 30}}>
+              SHARE
+            </Text>
+          </TouchableHighlight>
+            
+        </View>
+
+      </View>
+    );
+  }
+
+
 }
