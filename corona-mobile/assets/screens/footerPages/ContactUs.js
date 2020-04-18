0512f9894ecb3330f1a47485cdc69f9529c0bfeb
Michael Ginn <milespergallon271@gmail.com>
2020-04-18 15:50:15 -0500

    fix contact page styling

--- corona-mobile/assets/screens/footerPages/ContactUs.js
+++ corona-mobile/assets/screens/footerPages/ContactUs.js
@@ -7,47 +7,23 @@ import { SourceItem } from "../../components/FooterComponents";
 import { WebView } from "react-native-webview";
 import { Entypo } from "@expo/vector-icons";
 import { StandardText } from "./../../components/Texts.js";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function ContactUs({ route, navigation }) {
   const { styles, colors, isDark } = useStyle(
     "container",
     "boxContainer",
-    "subtitle"
+    "subtitle",
+    "scrollViewContent"
   );
 
   return (
-    <View style={styles.container}>
-      <View
-        style={{
-          marginTop: 100,
-          paddingHorizontal: 15,
-          marginLeft: 10,
-          marginRight: 10,
-          marginBottom: 10,
-          flexDirection: "row"
-        }}
-      >
-        <View style={{ flex: 1 }}>
-          <StandardText
-            fontSize="title"
-            isBold
-            allowFontScaling={false}
-            style={{
-              marginBottom: 10
-            }}
-          >
-            Contact Us
-          </StandardText>
-          <StandardText allowFontScaling={false}>
+    <ScrollView style={styles.container} contentContainerStyle={styles.scrollViewContent}>
+     
+          <StandardText>
             Find our email or fill out the form to get in touch with the team.
           </StandardText>
-        </View>
-        {/* <Entypo
-          name="mail"
-          size={100}
-          color={!isDark ? colors.textcolor : "#444"} /> */}
-      </View>
-      <View style={{ height: 440, marginBottom: 10 }}>
+
         <WebView
           javaScriptEnabled={true}
           domStorageEnabled={true}
@@ -57,9 +33,8 @@ export default function ContactUs({ route, navigation }) {
             uri:
               "https://us19.list-manage.com/survey?u=31c07122a61d1793401ba033b&id=869072057c"
           }}
-          style={{ flex: 1 }}
+          style={{ height: 480, marginVertical: 20, borderRadius: 5 }}
         />
-      </View>
       <View style={[styles.boxContainer, { marginBottom: 20 }]}>
         <Text style={[styles.subtitle, { marginTop: 0 }]}>
           For app-related inquiries reach out to us at
@@ -71,6 +46,6 @@ export default function ContactUs({ route, navigation }) {
           For press inquiries please reach out to satvik@runawayapp.com.
         </Text>
       </View>
-    </View>
+    </ScrollView>
   );
 }
