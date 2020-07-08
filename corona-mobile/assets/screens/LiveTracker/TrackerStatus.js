56937635e4005d555a6269cbe5547eb2bdd3dbb6
kathleenlund <kathleen.lund@wustl.edu>
2020-07-08 15:20:36 -0500

    Disclaimer pops up on each new open of app

--- corona-mobile/assets/screens/LiveTracker/TrackerStatus.js
+++ corona-mobile/assets/screens/LiveTracker/TrackerStatus.js
@@ -5,7 +5,6 @@ import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 import { WebView } from "react-native-webview";
 import { Entypo } from "@expo/vector-icons";
 import Toast from "react-native-root-toast";
-import AsyncStorage from "@react-native-community/async-storage";
 
 export default function TrackerStatus({ route, navigation }) {
   let webview;
@@ -14,43 +13,19 @@ export default function TrackerStatus({ route, navigation }) {
     navigation.na;
   };
 
-  firstLoad = async () => {
-    try {
-      const firstLoad = await AsyncStorage.getItem("@first_open");
-      console.log(firstLoad);
-      if (firstLoad === null) {
-        // First open of app
-        Toast.show(
-          "Disclaimer: Any ads or donation links on this page are not affiliated with Project Covid.",
-          {
-            duration: Toast.durations.LONG,
-            backgroundColor: "#43a047",
-            shadow: true,
-            animation: true,
-            hideOnPress: true,
-            delay: 0,
-            position: -100,
-          }
-        );
-        storeData("Done");
-      }
-    } catch (e) {
-      // Error reading value
-      console.log(e);
-    }
-  };
-
-  const storeData = async (value) => {
-    try {
-      await AsyncStorage.setItem("@first_open", value);
-    } catch (e) {
-      // Saving error
-      console.log(e);
-    }
-  };
-
   React.useEffect(() => {
-    firstLoad();
+    Toast.show(
+      "Disclaimer: Any ads or donation links on this page are not affiliated with Project Covid.",
+      {
+        duration: Toast.durations.LONG,
+        backgroundColor: "#43a047",
+        shadow: true,
+        animation: true,
+        hideOnPress: true,
+        delay: 0,
+        position: -100,
+      }
+    );
   });
 
   React.useLayoutEffect(() => {
